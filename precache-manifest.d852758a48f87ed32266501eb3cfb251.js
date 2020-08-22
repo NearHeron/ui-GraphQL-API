433b83136cbd19ba9989028afa1accc83435211c
Daniil Tsyplyackov <dtsyplyackov@yandex.ru>
2020-08-22 18:50:08 +0300

    Updates

---
+++ precache-manifest.d852758a48f87ed32266501eb3cfb251.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7ef43d52b8664d4fae41ce251738a2",
+    "url": "/ui-graphql-api/index.html"
+  },
+  {
+    "revision": "5112749689e63bc63fc5",
+    "url": "/ui-graphql-api/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7fb6cabe09275847e29f",
+    "url": "/ui-graphql-api/static/js/2.8bc26eb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ui-graphql-api/static/js/2.8bc26eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5112749689e63bc63fc5",
+    "url": "/ui-graphql-api/static/js/main.756783da.chunk.js"
+  },
+  {
+    "revision": "019be8c643d7ed1112d7",
+    "url": "/ui-graphql-api/static/js/runtime-main.5bc95d1b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ui-graphql-api/static/media/logo.5d5d9eef.svg"
+  }
+]);
